00f95efc447d605cba2b268f78f37cf96faf2933
Адитья <78430010+itzrexmodz@users.noreply.github.com>
2023-11-24 17:49:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 class Config:
-
     API_ID = os.getenv("API_ID")
     API_HASH = os.getenv("API_HASH")
     BOT_TOKEN = os.getenv("BOT_TOKEN")
